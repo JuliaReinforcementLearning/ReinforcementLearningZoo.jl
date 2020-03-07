ca196277ef0bc4a9e8e19115716a5d7b86737820
apprenticearnab <alipurarnabgems@gmail.com>
2020-03-07 22:43:09 +0530

    Adding Actor Network for DDPG Algorithm

--- test/actor.jl
+++ test/actor.jl
@@ -1,14 +1,15 @@
 using Flux
 using Zygote
 
-"""struct Actor
-   state_size::Int8
-   action_size::Int64
-   layer_size::Array{Int32,1,3}
-   learning_rate::Float64
-   batch_size::Int32
-   tau::Float64
-    Connstructor for the Actor Network
+"""mutable struct Actor
+           state_size::Int8
+           action_size::Int64
+           hidden_units::Array{Int32,1,3}
+           learning_rate::Float64
+           batch_size::Int32
+           tau::Float64
+   end"""
+   """ Connstructor for the Actor Network
      state_size:An integer denoting the dimensionality of the states
             in the current problem
      action_size: An integer denoting the dimensionality of the
@@ -21,14 +22,14 @@ using Zygote
       tau: A flot denoting the rate at which the target model will
             track the main model. Formally, the tracking function is defined as:
               target_weights = tau * main_weights + (1 - tau) * target_weights 
-end"""
-Actor1 = [5 6 4]
+Here as the whole model is not fully developed so we are tesing with random arrays"""
+Actor1 = [5 6 4]   #Actor.hiddden_units
 dict1 = Dict("W" => rand(Actor1[2],Actor1[1]),"b"=> rand(Actor1[2]))
 dict2 = Dict("W" => rand(Actor1[3],Actor1[2]),"b"=> rand(Actor1[3]))
 dict5 = Dict("W" => rand(Actor1[2],Actor1[1]),"b"=> rand(Actor1[2]))
 dict6 = Dict("W" => rand(Actor1[3],Actor1[1]),"b"=> rand(Actor1[3]))
-x = rand(5)
-y = rand(4)
+x = rand(5)  #x = state Actor.state_size = 5
+y = rand(4)  #y = action #Actor.action_size = 4
 function generate_model(x, W1 ,W2,b1,b2)
       """ Generates the model based on the hyperparameters defined in the constructor"""
        function sigmoid(x)
@@ -59,25 +60,17 @@ function generate_model(x, W1 ,W2,b1,b2)
 end
 function train(x,dict1,dict2,dict5,dict6)
          """Updates the weights of the main network
-            states: The states of the input to the network
-            action_gradients: The gradients of the actions to update the
-            network"""
-              "model = Flux.Chain(Dense(6, 64, relu),Dense(64, 16, relu),Dense(16, 1, relu));
-              x = rand(Bool, 14*16+4)
-              y = 100"
-              "opt = ADAM(params(model))
-              Flux.@epochs 100 Flux.train!(loss, [(x,y)], opt)
-         model1,model1_weight,model1_input = generate_model()"
+            by gradient descent and of the target network by target_weights = tau * main_weights + (1 - tau) * target_weights """
             for i in 1:100
                  model,l1,l2,k1 = generate_model(x,dict1["W"],dict2["W"],dict1["b"],dict2["b"])
                  target_model,l3,l4,k2 = generate_model(x ,dict5["W"],dict6["W"],dict5["b"],dict6["b"]) 
             
-                 dict1["W"] .-= 0.001 .* l1["gW"]
+                 dict1["W"] .-= 0.001 .* l1["gW"]     #Actor.learning_rate=0.001
                  dict1["b"] .-= 0.001 .* l1["gb"]
                  dict2["W"] .-= 0.001 .* l2["gW"]
                  dict2["b"] .-= 0.001 .* l2["gb"]
 
-                 dict5["W"] = 0.01 .* dict1["W"] .+ (1- 0.01) .* dict5["W"]
+                 dict5["W"] = 0.01 .* dict1["W"] .+ (1- 0.01) .* dict5["W"]   #Actor.tau = 0.01
                  dict5["b"] = 0.01 .* dict1["b"] .+ (1- 0.01) .* dict5["b"]
                  dict6["W"] = 0.01 .* dict2["W"] .+ (1- 0.01) .* dict6["W"]
                  dict6["b"] = 0.01 .* dict2["b"] .+ (1- 0.01) .* dict6["b"]
