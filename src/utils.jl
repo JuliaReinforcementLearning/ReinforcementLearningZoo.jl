31a111824b6ee775e2ed50403400b018ee5edbb7
Jun Tian <tianjun.cpp@gmail.com>
2020-05-16 21:27:25 +0800

    bugfix

--- src/utils.jl
+++ src/utils.jl
@@ -1,6 +1,6 @@
 export load_policy
 
-using Pkg
+using Pkg.Artifacts
 
 function load_policy(s::String, T=AbstractPolicy)
     if isfile(s)
@@ -8,7 +8,7 @@ function load_policy(s::String, T=AbstractPolicy)
     elseif isdir(s)
         load_policy(joinpath(s, "policy.bson"), T)
     else
-        dir = Pkg.@artifact_str s
+        dir = ensure_artifact_installed(s, find_artifacts_toml(@__DIR__))
         return load_policy(dir, T)
     end
 end
