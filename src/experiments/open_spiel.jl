9d8d1080b1be0a68def653fbb5fbd980462be063
Jun Tian <tianjun.cpp@gmail.com>
2020-08-25 23:30:26 +0800

    add Experiment for Minimax

--- src/experiments/open_spiel.jl
+++ src/experiments/open_spiel.jl
@@ -1,14 +1,17 @@
 function RLCore.Experiment(
     ::Val{:JuliaRL},
     ::Val{:Minimax},
-    ::Val{:OpenSpielEnv},
+    ::Val{:OpenSpiel},
     game;
 )
-    env = OpenSpielEnv(game)
+    env = OpenSpielEnv(string(game))
     agents = (
         Agent(policy=MinimaxPolicy(), role=0),
         Agent(policy=MinimaxPolicy(), role=1)
     )
     hooks = (TotalRewardPerEpisode(), TotalRewardPerEpisode())
-    Experiment(agents, env, StopAfterEpisode(1), hooks)
+    description="""
+    # Play `$game` in OpenSpiel with Minimax
+    """
+    Experiment(agents, env, StopAfterEpisode(1), hooks, description)
 end
