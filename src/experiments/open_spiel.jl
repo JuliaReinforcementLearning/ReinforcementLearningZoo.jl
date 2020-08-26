8b8e47c840eacc9d87aa94ab81857380e03bbe7d
Jun Tian <tianjun.cpp@gmail.com>
2020-08-26 15:48:08 +0800

    add Experiment for CFRPolicy

--- src/experiments/open_spiel.jl
+++ src/experiments/open_spiel.jl
@@ -15,3 +15,28 @@ function RLCore.Experiment(
     """
     Experiment(agents, env, StopAfterEpisode(1), hooks, description)
 end
+
+function RLCore.Experiment(
+    ::Val{:JuliaRL},
+    ::Val{:CFRPolicy},
+    ::Val{:OpenSpiel},
+    game;
+    n_inter=300
+)
+    env = OpenSpielEnv(game;default_state_type=RLBase.Information{String}, is_chance_agent_required=true)
+    π = CFRPolicy(;n_iter=n_iter, env)
+
+    agents = map(get_players(env)) do p
+        if p == get_chance_player(env)
+            Agent(;policy=RandomPolicy(), role=p)
+        else
+            Agent(;policy=π,role=p)
+        end
+    end
+
+    hooks = (TotalRewardPerEpisode() for _ in get_players())
+    description="""
+    # Play `$game` in OpenSpiel with CFRPolicy
+    """
+    Experiment(agents, env, StopAfterEpisode(1), hooks, description)
+end
