22670974a1641efe75c013a29da4f2918db885a7
norci <norci@users.noreply.github.com>
2020-09-16 13:09:53 +0800

    updated logging.

--- src/experiments/rl_envs.jl
+++ src/experiments/rl_envs.jl
@@ -1216,25 +1216,24 @@ function RLCore.Experiment(
             state_size = (ns,),
         ),
     )
-
-    stop_condition = StopAfterStep(30_000)
+    # PG is updated after each episode
+    stop_condition = StopAfterEpisode(500)
 
     total_reward_per_episode = TotalRewardPerEpisode()
     time_per_step = TimePerStep()
     hook = ComposedHook(
         total_reward_per_episode,
         time_per_step,
-        # PG is updated at the end of an episode, so don't log per step loss
         DoEveryNEpisode() do t, agent, env
             with_logger(lg) do
                 @info(
                     "training",
                     loss = agent.policy.loss,
-                    reward = total_reward_per_episode.rewards[end]
+                    reward = total_reward_per_episode.rewards[end],
                 )
             end
         end,
-        DoEveryNStep(30_000) do t, agent, env
+        DoEveryNEpisode(500) do t, agent, env
             RLCore.save(save_dir, agent)
             BSON.@save joinpath(save_dir, "stats.bson") total_reward_per_episode time_per_step
         end,
@@ -1284,24 +1283,29 @@ function RLCore.Experiment(
         ),
     )
 
-    stop_condition = StopAfterStep(20_000)
+    stop_condition = StopAfterStep(50_000)
 
     total_reward_per_episode = TotalRewardPerEpisode()
     time_per_step = TimePerStep()
     hook = ComposedHook(
         total_reward_per_episode,
         time_per_step,
-        # PG is updated at the end of an episode, so don't log per step loss
+        DoEveryNStep() do t, agent, env
+            with_logger(lg) do
+                @info "training" # in order to increase the step count in tb logger
+            end
+        end,
         DoEveryNEpisode() do t, agent, env
             with_logger(lg) do
                 @info(
                     "training",
                     loss = agent.policy.loss,
-                    reward = total_reward_per_episode.rewards[end]
+                    reward = total_reward_per_episode.rewards[end],
+                    log_step_increment = 0
                 )
             end
         end,
-        DoEveryNStep(10_000) do t, agent, env
+        DoEveryNStep(50_000) do t, agent, env
             RLCore.save(save_dir, agent)
             BSON.@save joinpath(save_dir, "stats.bson") total_reward_per_episode time_per_step
         end,
