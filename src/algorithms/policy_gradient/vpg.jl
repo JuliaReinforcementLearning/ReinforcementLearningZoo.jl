fed721f6a7e7931895dc40ca28d618a2ac8c20d1
norci <norci@users.noreply.github.com>
2020-09-19 17:15:20 +0800

    added mini batch in vpg

--- src/algorithms/policy_gradient/vpg.jl
+++ src/algorithms/policy_gradient/vpg.jl
@@ -1,6 +1,6 @@
 using Distributions: Categorical, Normal
-using Flux: stack, unstack, chunk, normalise
-using Random
+using Flux: normalise
+using Random: shuffle
 
 using ReinforcementLearningBase
 using ReinforcementLearningCore
@@ -21,7 +21,7 @@ Base.@kwdef mutable struct VPGPolicy{
     γ::Float32 = 0.99f0 # discount factor
     α_θ = 1.0f0 # step size of policy
     α_w = 1.0f0 # step size of baseline
-    batch_size::Int = 128
+    batch_size::Int = 1024
     rng::R = Random.GLOBAL_RNG
     loss::Float32 = 0.0f0
     baseline_loss::Float32 = 0.0f0
@@ -94,45 +94,44 @@ function RLBase.update!(π::VPGPolicy, traj::ElasticCompactSARTSATrajectory, ::D
     model = π.approximator
     to_dev(x) = send_to_device(device(model), x)
 
-    n_chunks = ceil(Int, length(traj[:terminal]) / π.batch_size)
-    to_chunks(x) = chunk(x, n_chunks)
+    states = traj[:state]
+    actions = traj[:action]
+    gains = traj[:reward] |> x -> discount_rewards(x, π.γ)
 
-    # TODO: use mini batches.
+    for idx in Iterators.partition(shuffle(1:length(traj[:terminal])), π.batch_size)
+        S = states[:, idx] |> to_dev
+        A = actions[idx]
+        G = gains[idx] |> x -> Flux.unsqueeze(x, 1) |> to_dev
+        # gains is a 1 colomn array, but the ouput of flux model is 1 row, n_batch columns array. so unsqueeze it.
 
-    states = traj[:state] |> to_dev #|> x -> stack.(to_chunks(unstack(x, 2)), 2)
-    actions = traj[:action] #|> to_chunks
-    # gains is a 1 colomn array, but the ouput of flux model is 1 row, n_batch columns array. so unsqueeze it.
-    gains =
-        traj[:reward] |>
-        x -> discount_rewards(x, π.γ) |> x -> Flux.unsqueeze(x, 1) |> to_dev #|> to_chunks
+        if typeof(π.baseline) <: NeuralNetworkApproximator
+            δ = G - π.baseline(S)
+            gs = gradient(Flux.params(π.baseline)) do
+                # TODO: is the loss function correct?
+                loss = mse(π.baseline(S), G) * π.α_w
+                ignore() do
+                    π.baseline_loss = loss
+                end
+                loss
+            end
+            update!(π.baseline, gs)
+        end
+        if typeof(π.baseline) <: Nothing
+            # normalise should not be used with baseline. or the loss of the policy will be too small.
+            δ = G |> x -> normalise(x; dims = 2)
+        end
 
-    if typeof(π.baseline) <: NeuralNetworkApproximator
-        δ = gains - π.baseline(states)
-        gs = gradient(Flux.params(π.baseline)) do
-            # TODO: is the loss function correct?
-            loss = mse(π.baseline(states), gains) * π.α_w
+        gs = gradient(Flux.params(model)) do
+            log_prob = S |> model |> logsoftmax
+            log_probₐ = log_prob[CartesianIndex.(A, 1:length(A))]
+            loss = -mean(log_probₐ .* δ) * π.α_θ
             ignore() do
-                π.baseline_loss = loss
+                π.loss = loss
             end
             loss
         end
-        update!(π.baseline, gs)
+        update!(model, gs)
     end
-    if typeof(π.baseline) <: Nothing
-        # normalise should not be used with baseline. or the loss of the policy will be too small.
-        δ = gains |> x -> normalise(x; dims = 2)
-    end
-
-    gs = gradient(Flux.params(model)) do
-        log_prob = states |> model |> logsoftmax
-        log_probₐ = log_prob[CartesianIndex.(actions, 1:length(actions))]
-        loss = -mean(log_probₐ .* δ) * π.α_θ
-        ignore() do
-            π.loss = loss
-        end
-        loss
-    end
-    update!(model, gs)
     empty!(traj)
 end
 
