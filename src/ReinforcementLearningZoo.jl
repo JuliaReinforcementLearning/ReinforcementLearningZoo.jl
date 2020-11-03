1edf3f183a1b0e4898eb8b720f0de63e0bc05450
Sid-Bhatia-0 <sidb60200@gmail.com>
2020-11-03 13:27:19 +0530

    fix: GridWorlds not yet present in RLEnvs

--- src/ReinforcementLearningZoo.jl
+++ src/ReinforcementLearningZoo.jl
@@ -21,8 +21,9 @@ function __init__()
         @require ArcadeLearningEnvironment = "b7f77d8d-088d-5e02-8ac0-89aab2acc977" include("experiments/atari.jl")
         @require SnakeGames = "34dccd9f-48d6-4445-aa0f-8c2e373b5429" include("experiments/snake.jl")
         @require OpenSpiel = "ceb70bd2-fe3f-44f0-b81f-41608acaf2f2" include("experiments/open_spiel.jl")
-        @require GridWorlds = "e15a9946-cd7f-4d03-83e2-6c30bacb0043" include("experiments/gridworlds.jl")
     end
+
+    @require GridWorlds = "e15a9946-cd7f-4d03-83e2-6c30bacb0043" include("experiments/gridworlds.jl")
 end
 
 end # module
