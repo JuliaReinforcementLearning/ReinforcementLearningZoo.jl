2edbf33ddf7b8dbd18d16085883d08bcdcce1201
Sid-Bhatia-0 <sidb60200@gmail.com>
2020-11-11 19:17:47 +0530

    add JuliaRL_BasicDQN_FourRooms experiment

--- src/experiments/gridworlds.jl
+++ src/experiments/gridworlds.jl
@@ -95,3 +95,87 @@ function RLCore.Experiment(
 
     Experiment(agent, env, stop_condition, hook, description)
 end
+
+function RLCore.Experiment(
+    ::Val{:JuliaRL},
+    ::Val{:BasicDQN},
+    ::Val{:FourRooms},
+    ::Nothing;
+    save_dir = nothing,
+    seed = 123,
+)
+    if isnothing(save_dir)
+        t = Dates.format(now(), "yyyy_mm_dd_HH_MM_SS")
+        save_dir = joinpath(pwd(), "checkpoints", "JuliaRL_BasicDQN_FourRooms_$(t)")
+    end
+
+    lg = TBLogger(joinpath(save_dir, "tb_log"), min_level = Logging.Info)
+    rng = StableRNG(seed)
+
+    inner_env = GridWorlds.FourRooms()
+
+    integer_actions_wrapper = ActionTransformedEnv(a -> get_actions(inner_env)[a]; mapping = x -> Dict(action => i for (i, action) in enumerate([get_actions(inner_env)...]))[x])
+
+    env = inner_env |> integer_actions_wrapper
+
+    ns, na = length(get_state(env)), length(get_actions(env))
+    agent = Agent(
+        policy = QBasedPolicy(
+            learner = BasicDQNLearner(
+                approximator = NeuralNetworkApproximator(
+                    model = Chain(
+                        Dense(ns, 128, relu; initW = glorot_uniform(rng)),
+                        Dense(128, na; initW = glorot_uniform(rng)),
+                    ) |> cpu,
+                    optimizer = ADAM(),
+                ),
+                batch_size = 32,
+                min_replay_history = 100,
+                loss_func = huber_loss,
+                rng = rng,
+            ),
+            explorer = EpsilonGreedyExplorer(
+                kind = :exp,
+                ϵ_stable = 0.01,
+                decay_steps = 500,
+                rng = rng,
+            ),
+        ),
+        trajectory = CircularCompactSARTSATrajectory(
+            capacity = 1000,
+            reward_type = Float64,
+            state_type = Float64,
+            state_size = (ns,),
+        ),
+    )
+
+    stop_condition = StopAfterStep(1000)
+
+    total_reward_per_episode = TotalRewardPerEpisode()
+    time_per_step = TimePerStep()
+    hook = ComposedHook(
+        total_reward_per_episode,
+        time_per_step,
+        DoEveryNStep() do t, agent, env
+            with_logger(lg) do
+                @info "training" loss = agent.policy.learner.loss
+            end
+        end,
+        DoEveryNEpisode() do t, agent, env
+            with_logger(lg) do
+                @info "training" reward = total_reward_per_episode.rewards[end] log_step_increment =
+                    0
+            end
+        end,
+        DoEveryNStep(10000) do t, agent, env
+            RLCore.save(save_dir, agent)
+            BSON.@save joinpath(save_dir, "stats.bson") total_reward_per_episode time_per_step
+        end,
+    )
+
+    description = """
+    The environment is FourRooms.
+    """
+
+    Experiment(agent, env, stop_condition, hook, description)
+end
