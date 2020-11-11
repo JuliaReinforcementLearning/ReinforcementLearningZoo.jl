1305809c45b47f39824fde72022307baae2d2634
Sid-Bhatia-0 <sidb60200@gmail.com>
2020-11-11 19:13:18 +0530

    clean comment

--- src/experiments/gridworlds.jl
+++ src/experiments/gridworlds.jl
@@ -41,7 +41,6 @@ function RLCore.Experiment(
                 approximator = NeuralNetworkApproximator(
                     model = Chain(
                         Dense(ns, 128, relu; initW = glorot_uniform(rng)),
-                        # Dense(128, 128, relu; initW = glorot_uniform(rng)),
                         Dense(128, na; initW = glorot_uniform(rng)),
                     ) |> cpu,
                     optimizer = ADAM(),
