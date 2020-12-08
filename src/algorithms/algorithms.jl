5594f63849be71b5302250fc967dd7e44369431b
Jun Tian <tianjun.cpp@gmail.com>
2020-12-08 23:18:09 +0800

    move using into RLZoo.jl

--- src/algorithms/algorithms.jl
+++ src/algorithms/algorithms.jl
@@ -1,18 +1,3 @@
-using Random
-using Random: shuffle
-using CUDA
-using Zygote
-using Zygote: ignore
-using Flux
-using Flux: onehot, normalise
-using Flux.Losses: mse
-using StatsBase
-using StatsBase: sample, Weights, mean
-using LinearAlgebra: dot
-using MacroTools
-using Distributions: Categorical, Normal, logpdf
-using StructArrays
-
 include("dqns/dqns.jl")
 include("policy_gradient/policy_gradient.jl")
 include("searching/searching.jl")
