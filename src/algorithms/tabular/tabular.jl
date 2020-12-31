8e82f865f80d3d1f85003ac49d863dd7457501ee
Jun Tian <tianjun.cpp@gmail.com>
2021-01-01 00:15:47 +0800

    sync changes related to RLIntro

--- src/algorithms/tabular/tabular.jl
+++ src/algorithms/tabular/tabular.jl
@@ -1,1 +1,3 @@
 include("monte_carlo_learner.jl")
+include("td_learner.jl")
+include("gradient_bandit_learner.jl")
---
+++ src/algorithms/tabular/td_learner.jl
@@ -0,0 +1,107 @@
+export TDLearner
+
+using LinearAlgebra:dot
+using Distributions:pdf
+
+Base.@kwdef struct TDLearner{A} <: AbstractLearner
+    approximator::A
+    γ::Float64
+    method::Symbol
+    n::Int
+end
+
+(L::TDLearner)(env::AbstractEnv) = L.approximator(state(env))
+(L::TDLearner)(s) = L.approximator(s)
+(L::TDLearner)(s, a) = L.approximator(s, a)
+
+## update policies
+
+function RLBase.update!(L::TDLearner, t::AbstractTrajectory, ::AbstractEnv, s::Union{PreActStage, PostEpisodeStage})
+    _update!(L, L.approximator, Val(L.method), t, s)
+end
+
+## update trajectories
+
+function RLBase.update!(
+    t::AbstractTrajectory,
+    ::Union{QBasedPolicy{<:TDLearner}, NamedPolicy{<:QBasedPolicy{<:TDLearner}}},
+    ::AbstractEnv,
+    ::PreEpisodeStage
+)
+    empty!(t)
+end
+
+## implementations
+
+function _update!(
+    L::TDLearner,
+    ::TabularQApproximator,
+    ::Val{:SARSA},
+    t::Trajectory,
+    ::PreActStage
+)
+    S, A, R, T = [t[x] for x in SART]
+    n, γ, Q = L.n, L.γ, L.approximator
+
+    if length(R) >= n+1
+        s, a, s′, a′ = S[end-n-1], A[end-n-1], S[end], A[end]
+        G = discount_rewards_reduced(@view(R[end-n:end]), γ) + γ^(n+1) * Q(s′, a′)
+        update!(Q, (s,a) => Q(s ,a) - G)
+    end
+end
+
+function _update!(
+    L::TDLearner,
+    ::TabularQApproximator,
+    ::Union{Val{:SARSA}, Val{:ExpectedSARSA}},
+    t::Trajectory,
+    ::PostEpisodeStage
+)
+    S, A, R, T = [t[x] for x in SART]
+    n, γ, Q = L.n, L.γ, L.approximator
+    G = 0.
+    for i in 1:min(n+1, length(R))
+        G = R[end-i+1] + γ * G
+        s, a = S[end-i], A[end-i]
+        update!(Q, (s,a) => Q(s, a) - G)
+    end
+end
+
+function _update!(
+    L::TDLearner,
+    ::TabularQApproximator,
+    ::Val{:ExpectedSARSA},
+    experience,
+    ::PreActStage
+)
+    t, p = experience
+
+    S = t[:state]
+    A = t[:action]
+    R = t[:reward]
+
+    n, γ, Q = L.n, L.γ, L.approximator
+
+    if length(R) >= n+1
+        s, a, s′ = S[end-n-1], A[end-n-1], S[end]
+        G = discount_rewards_reduced(@view(R[end-n:end]), γ) + γ^(n+1) * dot(Q(s′), p)
+        update!(Q, (s,a) => Q(s ,a) - G)
+    end
+end
+
+function _update!(
+    L::TDLearner,
+    ::TabularQApproximator,
+    ::Val{:ExpectedSARSA},
+    t::AbstractTrajectory,
+    ::PreActStage
+)
+    S, A, R, T = [t[x] for x in SART]
+    n, γ, Q = L.n, L.γ, L.approximator
+
+    if length(R) >= n+1
+        s, a, s′ = S[end-n-1], A[end-n-1], S[end]
+        G = discount_rewards_reduced(@view(R[end-n:end]), γ) + γ^(n+1) * maximum(Q(s′))
+        update!(Q, (s,a) => Q(s ,a) - G)
+    end
+end
