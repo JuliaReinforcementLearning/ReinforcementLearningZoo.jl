66bd87e7c23aa4c169678d2a007dcbda98c5b59f
Jun Tian <tianjun.cpp@gmail.com>
2021-02-02 21:07:54 +0800

    add experiment for bc

--- src/algorithms/offline_rl/behavior_cloning.jl
+++ src/algorithms/offline_rl/behavior_cloning.jl
@@ -1,39 +1,33 @@
-export BehaviorCloingPolicy
-
-# https://github.com/FluxML/Flux.jl/pull/1492
-# TODO: use OneHotMatrix with logitcrossentropy instread
-function logit_sparse_crossentropy(ŷ, y; agg=mean)
-    agg(.-logsoftmax(ŷ)[CartesianIndex.(y, 1:length(y))])
-end
+export BehaviorCloningPolicy
 
 """
-    BehaviorCloingPolicy(;kw...)
+    BehaviorCloningPolicy(;kw...)
 
 # Keyword Arguments
 
 - `approximator`: calculate the logits of possible actions directly
-- `loss_func=logit_sparse_crossentropy`
 - `explorer=GreedyExplorer()` 
 
 """
-Base.@kwdef struct BehaviorCloingPolicy{A} <: AbstractPolicy
+Base.@kwdef struct BehaviorCloningPolicy{A} <: AbstractPolicy
     approximator::A
-    loss_func::Any = logit_sparse_crossentropy
     explorer::Any = GreedyExplorer()
 end
 
-function (p::BehaviorCloingPolicy)(env::AbstractEnv)
+function (p::BehaviorCloningPolicy)(env::AbstractEnv)
     s = state(env)
     s_batch = Flux.unsqueeze(s, ndims(s) + 1)
     logits = p.approximator(s_batch) |> vec  # drop dimension
     p.explorer(logits)
 end
 
-function RLBase.update!(p::BehaviorCloingPolicy, batch::NamedTuple{(:state, :action)})
+function RLBase.update!(p::BehaviorCloningPolicy, batch::NamedTuple{(:state, :action)})
     s, a = batch.state, batch.action
-    m, loss_func = p.approximator, p.loss_func
+    m = p.approximator
     gs = gradient(params(m)) do
-        loss_func(m(s), a)
+        ŷ = m(s)
+        y = Flux.onehotbatch(a, axes(ŷ, 1))
+        logitcrossentropy(ŷ, y)
     end
     update!(m, gs)
 end
